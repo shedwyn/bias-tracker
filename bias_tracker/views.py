53998ac52468666fae8fb6c953cb288c242c1c27
elfough <shedwyn@gmail.com>
2016-05-15 13:30:05 -0700

    laid out the basic views, no code

--- bias_tracker/views.py
+++ bias_tracker/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from . import logic
 
 
-def render_login_page():
+def render_login_page(request):
     """renders the home login page.  submitting the log in will redirect to
     menu-page"""
     pass
 
 
-def render_menu_page():
+def render_menu_page(request):
     """'home' page after log-in.  user has four choices:  create new incident
     edit existing incident, see statistics, or log out. 'submit' will direct
     to appropriate page'"""
@@ -18,20 +18,20 @@ def render_menu_page():
     pass
 
 
-def render_new_incident_log_page():
+def render_new_incident_log_page(request):
     """renders incident log page.  user enters field data for database.
     'submit' sends data to db and redirects to menu-page"""
     pass
 
 
-def render_edit_incident_log_page():
+def render_edit_incident_log_page(request):
     """takes incident id, finds incident in db, fills fields with data for that
     incident.  user may change data and submit edited record.  'submit' sends
     data to db and redirects to menu-page"""
     pass
 
 
-def render_statistics_view_page():
+def render_statistics_view_page(request):
     """renders statistics view page - auto loads user's statistics on top
     half of page, but allow single field choice for lower half.  When user
     selects iSubject, reload lower page with stats for that iSubject
