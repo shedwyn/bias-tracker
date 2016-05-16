7d1d3764567be9be2aa95d07ddc4a7ad627137ca
elfough <shedwyn@gmail.com>
2016-05-16 11:01:25 -0700

    basic logic for login page done

--- bias_tracker/logic.py
+++ bias_tracker/logic.py
@@ -1,25 +1,46 @@
-# from . import bias_tracker
+from . import views
 
-# class Person:
-#     """name of person assigned to unique ID"""
-#     def __init__(self, name):
-#         self.name = name
+# from django.db import models
+#
+#
+# class User(models.Model):
+#     user_name = models.CharField()
+#     email = models.CharField()
+#     sign_up_date = models.DateTimeField()
 
 
-# LOGIN PAGE #
-def testing_write_methods():
-    """just testing if i can write to csv file"""
-    human_database = {
-        'erin': ("Snow White", 'F'),
-        'grumpy': ('Grumpy Dwarf', 'M'),
-        # 'sneezy': 'Sneezy Dwarf', 'M',
-        # 'bashful': 'Bashful Dwarf', 'M'
-    }
-    with open('database.txt', 'w') as humans_db_file:
-        for name, details in human_database.items():
-            line = '{} {}\n'.format(name, details)
-            humans_db_file.write(line)
+def fetch_current_names_db():
+    """return dictionary of instantiated user details"""
+    with open(database.txt, 'w') as humans_db_file:  # should this be 'r'?
+        user_database = humans_db_file.readlines()
+    # format into proper dictionary?
+    return user_database
 
+
+def check_id_in_db(user_id, user_database):
+    """take user_id and see if it is in the db of users, return bool val"""
+    if user_id in user_database:
+        return True
+    else:
+        return False
+
+
+def generate_error_message():
+    pass
+
+
+def get_db_name(user_id):
+    """receive user's login id and return user details for that login id"""
+    user_database = fetch_current_names_db()
+    id_validity = check_id_in_db(user_id, user_database)
+    if id_validity:
+        return user_id.name
+    else:
+        generate_error_message()
+        return views.render_login_page(request)
+
+
+# MENU PAGE #
 
 def write_more(key, values):
     with open(database.txt, 'w') as humans_db_file:
@@ -27,11 +48,6 @@ def write_more(key, values):
     print(human_database)
     human_database.write(key values)
     print(human_database)
-
-
-# MENU PAGE #
-
-
 
 
 # INCIDENT PAGE #
