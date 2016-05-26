2787f8dc49c2fd184cc2288935a0493470214c90
elfough <shedwyn@gmail.com>
2016-05-26 11:37:27 -0700

    fiddling with implementation

--- bias_tracker/logic.py
+++ bias_tracker/logic.py
@@ -6,39 +6,31 @@ from . import views
 #     self.text_description
 
 
-def fetch_current_names_db():
-    """return dictionary of instantiated user details"""
-    with open(database.txt, 'w') as humans_db_file:  # should this be 'r'?
-        user_database = humans_db_file.readlines()
-    # format into proper dictionary?
-    return user_database
-
-
-def check_id_in_db(user_id, user_database):
-    """take user_id and see if it is in the db of users, return bool val"""
-    if user_id in user_database:
-        return True
-    else:
-        return False
-
-
-def generate_error_message():
-    pass
-
-
-def get_db_name(user_id):
-    """receive user's login id and return user details for that login id"""
-    user_database = fetch_current_names_db()
-    id_validity = check_id_in_db(user_id, user_database)
-    if id_validity:
-        return user_id.name
-    else:
-        generate_error_message()
-        # what is this error message?  where does it appear on page?//
-        # probably need to add a warning alert on login page //
-        # possibly reverse the else consequences - return to login page
-        # with "lit" warning message?
-        return views.render_login_page(request)
+# def check_id_in_db(user_id, user_database):
+#     """take user_id and see if it is in the db of users, return bool val"""
+#     if user_id in user_database:
+#         return True
+#     else:
+#         return False
+#
+#
+# def generate_error_message():
+#     pass
+#
+#
+# def get_db_name(user_id):
+#     """receive user's login id and return user details for that login id"""
+#     user_database = fetch_current_names_db()
+#     id_validity = check_id_in_db(user_id, user_database)
+#     if id_validity = True:
+#         return user_id.name
+#     else:
+#         generate_error_message()
+#         # what is this error message?  where does it appear on page?//
+#         # probably need to add a warning alert on login page //
+#         # possibly reverse the else consequences - return to login page
+#         # with "lit" warning message?
+#         return views.render_login_page(request)
 
 
 # MENU PAGE #
--- bias_tracker/views.py
+++ bias_tracker/views.py
@@ -6,44 +6,45 @@ from . import logic
 def render_index_page(request):
     """renders the home login page.  submitting the log in will redirect to
     menu-page"""
-    return render(request, 'index.html', {})
-
-
-def render_menu_page(request):
-    """'home' page after log-in.  user has four choices:  create new incident
-    edit existing incident, see statistics, or log out. 'submit' will direct
-    to appropriate page'"""
-    # all other pages will return to this page if the original user //
-    # remains logged in
-    user_email_login = request.POST['login_name']
-    user_name = logic.get_db_name(user_email_login)
-    # take in email, see if it exists in db, if so, return user_name, if not//
-    # produce error message
-
     pass
+    # return render(request, 'bias_tracker/index.html', {})
 
 
-def render_new_incident_log_page(request):
-    """renders incident log page.  user enters field data for database.
-    'submit' sends data to db and redirects to menu-page"""
-    pass
-
-
-def render_edit_incident_log_page(request):
-    """takes incident id, finds incident in db, fills fields with data for that
-    incident.  user may change data and submit edited record.  'submit' sends
-    data to db and redirects to menu-page"""
-    pass
-
-
-def render_statistics_view_page(request):
-    """renders statistics view page - auto loads user's statistics on top
-    half of page, but allow single field choice for lower half.  When user
-    selects iSubject, reload lower page with stats for that iSubject
-    'return' button returns to menu-page"""
-    pass
-
-
-def render_add_new_isubject(request):
-    """may need page to add new iSubject"""
-    pass
+# def render_menu_page(request):
+#     """'home' page after log-in.  user has four choices:  create new incident
+#     edit existing incident, see statistics, or log out. 'submit' will direct
+#     to appropriate page'"""
+#     # all other pages will return to this page if the original user //
+#     # remains logged in
+#     user_email_login = request.POST['login_name']
+#     user_name = logic.get_db_name(user_email_login)
+#     # take in email, see if it exists in db, if so, return user_name, if not//
+#     # produce error message
+#
+#     pass
+#
+#
+# def render_new_incident_log_page(request):
+#     """renders incident log page.  user enters field data for database.
+#     'submit' sends data to db and redirects to menu-page"""
+#     pass
+#
+#
+# def render_edit_incident_log_page(request):
+#     """takes incident id, finds incident in db, fills fields with data for that
+#     incident.  user may change data and submit edited record.  'submit' sends
+#     data to db and redirects to menu-page"""
+#     pass
+#
+#
+# def render_statistics_view_page(request):
+#     """renders statistics view page - auto loads user's statistics on top
+#     half of page, but allow single field choice for lower half.  When user
+#     selects iSubject, reload lower page with stats for that iSubject
+#     'return' button returns to menu-page"""
+#     pass
+#
+#
+# def render_add_new_isubject(request):
+#     """may need page to add new iSubject"""
+#     pass
