1b602df2da0f94610ad66835265311693fd57910
elfough <shedwyn@gmail.com>
2016-06-04 22:25:59 -0700

    added fields to incident model

--- bias_tracker/models.py
+++ bias_tracker/models.py
@@ -1,5 +1,7 @@
 """bias_tracker Models configuration"""
 
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -41,41 +43,51 @@ class Incident(models.Model):
     'i_' used as abbreviation for 'incident'.
     """
 
+    default_description = 'describe incident detail here'
     TYPE_CHOICES = (
         ('Exclusion', 'Exclusion'),
         ('Inclusion', 'Inclusion')
     )
-    # foreing and manytomany need default keys so on_delete can be changed
+    # look into pre-delete and filling incidents with sub_id 1
     author = models.ForeignKey(User)
     subjects = models.ManyToManyField(Person)  # list of persons
-    # filing_date = models.DateTimeField('Filing Date')
-    # i_date = models.DateField('Incident Date')
-    # i_time = models.TimeField('Incident Time', blank=True)
-    incident_type = models.CharField(
-        'Incident Type',
-        choices=TYPE_CHOICES,
-        max_length=10
-    )
+    filing_date = models.DateField(auto_now_add=True)
+    incident_date = models.DateField(default=datetime.today)
+    incident_time = models.TimeField(blank=True)
+    incident_type = models.CharField(choices=TYPE_CHOICES, max_length=10)
     descriptors = models.ManyToManyField(Descriptor)
-    # text_description = models.TextField('Incident Description')
+    text_description = models.TextField(
+        'Incident Description',
+        default=default_description,
+    )
 
     def __str__(self):
         """docstring"""
-        return 'Incident_str({}, {}, {}, {}, {})'.format(
+        return 'Incident_str({}, {}, {}, {}, {}, {}, {}, {}, {})'.format(
             self.id,
             self.author,
+            self.filing_date,
+            self.incident_date,
+            self.incident_time,
             self.subjects.all(),
             self.incident_type,
-            self.descriptors.all()
+            self.descriptors.all(),
+            self.text_description
         )
         # self.filing_date, self.i_date, self.i_time, self.text_description
 
     def __repr__(self):
         """docstring"""
-        return 'Incident_repr({!r}, {!r}, {!r}, {!r}, {!r})'.format(
+        return 'Incident_repr(\
+            {!r}, {!r}, {!r}, {!r}, {!r}, {!r}, {!r}, {!r}, {!r}\
+        )'.format(
             self.id,
             self.author,
+            self.filing_date,
+            self.incident_date,
+            self.incident_time,
             self.subjects.all(),
             self.incident_type,
-            self.descriptors.all()
+            self.descriptors.all(),
+            self.text_description
         )
