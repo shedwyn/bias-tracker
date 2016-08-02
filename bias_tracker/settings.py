42040e6af4af579b94375dd0c9adcb2df5e02ebf
elfough <shedwyn@gmail.com>
2016-08-01 21:37:05 -0700

    adjusted settings.py - changed db settings

--- bias_tracker/settings.py
+++ bias_tracker/settings.py
@@ -83,22 +83,26 @@ WSGI_APPLICATION = 'bias_tracker.wsgi.application'
 
 TRACKER_DB_PASS = os.environ['TRACKER_DB_PASS']
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+# db_from_env = dj_database_url.config(conn_max_age=500)
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bias_tracker',
-        'USER': 'elfough',
-        'PASSWORD': 'TRACKER_DB_PASS',
-        'HOST': 'localhost',
-        'PORT': '5432',  # as per the settings in PostGreSQL
-    }
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'bias_tracker',
+#         'USER': 'elfough',
+#         'PASSWORD': 'TRACKER_DB_PASS',
+#         'HOST': 'localhost',
+#         'PORT': '5432',  # as per the settings in PostGreSQL
+#     }
+# }
+
+# DATABASES['default'].update(db_from_env)
+
+DATABASES = {  # deployment settings
+    'default': dj_database_url.config()
 }
-
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
